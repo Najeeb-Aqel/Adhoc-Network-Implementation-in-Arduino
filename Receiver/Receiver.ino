80e39d04499ba5a0d7368ea157bfd538123b5b7c
Ashraf Ghanem <ashraf.nadir3@gmail.com>
2019-04-16 23:49:26 +0300

    Adding sender and receiver to be tested
    Signed-off-by: Ashraf Ghanem <ashraf.nadir3@gmail.com>

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,21 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not used but needed to compile
+
+RH_ASK driver;
+
+void setup(){
+    Serial.begin(9600);  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop(){
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)){ // Non-blocking
+      int i;
+      // Message with a good checksum received, dump it.
+      //Serial.print("");
+      Serial.println((char*)buf);         
+    }
+}
---
+++ Sender/Sender.ino
@@ -0,0 +1,30 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not used but needed to compile
+
+RH_ASK driver;
+
+const int tmpSensorPin = A0;
+int reading;
+float tmp;
+
+void setup() {
+  Serial.begin(9600);    // Debugging only
+  if (!driver.init())
+    Serial.println("init failed");
+}
+
+void loop() {
+  readTmp();
+  //Serial.println(tmp);
+  char msg[20];
+  dtostrf(tmp, 5 , 3 , msg);
+  //sprintf(msg,"tmp=%s,distance=%s",msg,msg);
+  driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(1000);
+}
+
+void readTmp() {
+  reading = analogRead(tmpSensorPin);
+  tmp = (reading * 5.0 * 100 / 1023.0) - 50;
+}
