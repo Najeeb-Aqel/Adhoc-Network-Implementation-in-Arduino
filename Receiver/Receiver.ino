639e0cc0ba48cd5443d0f763f969d00d8ef123bc
Najeeb Aqel <najeebaqel@ieee.org>
2019-04-17 10:39:17 +0300

    Receiver updates

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -3,7 +3,10 @@
 
 RH_ASK driver;
 
+int res;
+
 void setup(){
+    pinMode(3,OUTPUT);
     Serial.begin(9600);  // Debugging only
     if (!driver.init())
          Serial.println("init failed");
@@ -12,10 +15,28 @@ void setup(){
 void loop(){
     uint8_t buf[12];
     uint8_t buflen = sizeof(buf);
+    
     if (driver.recv(buf, &buflen)){ // Non-blocking
       int i;
       // Message with a good checksum received, dump it.
       //Serial.print("");
-      Serial.println((char*)buf);         
+      Serial.println((char*)buf);
+      char result[2];
+      result[0]= buf[0];
+      result[1]= buf[1];
+
+      res = result[0] - '0';
+      res = (res*10) + result[1] - '0';
+
+      
+      Serial.println((char*)res);
     }
+
+    if (res > 20)
+      {
+        digitalWrite(3,HIGH);
+      }else
+      {
+        digitalWrite(3,LOW);
+      }
 }
