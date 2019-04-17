a4591622ec445485403c5a79557160db22438072
Ashraf Ghanem <ashraf.nadir3@gmail.com>
2019-04-17 10:40:29 +0300

    Sender code is modified
    Signed-off-by: Ashraf Ghanem <ashraf.nadir3@gmail.com>

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -8,6 +8,16 @@ int reading;
 float tmp;
 
 void setup() {
+  Serial.begin(9600);    // Debu#include <RH_ASK.h>
+#include <SPI.h> // Not used but needed to compile
+
+RH_ASK driver;
+
+const int tmpSensorPin = A0;
+int reading;
+float tmp;
+
+void setup() {
   Serial.begin(9600);    // Debugging only
   if (!driver.init())
     Serial.println("init failed");
@@ -15,9 +25,9 @@ void setup() {
 
 void loop() {
   readTmp();
-  //Serial.println(tmp);
   char msg[20];
-  dtostrf(tmp, 5 , 3 , msg);
+  dtostrf(tmp, 2 , 0 , msg);
+  Serial.println(msg);
   //sprintf(msg,"tmp=%s,distance=%s",msg,msg);
   driver.send((uint8_t *)msg, strlen(msg));
   driver.waitPacketSent();
